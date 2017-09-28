9b5f1b55b02186d39990c31c67955bb960946db2
Thiago Paes <mrprompt@gmail.com>
2017-09-28 01:31:15 -0300

    utilizando a biblioteca Narcoleptic para otimizar o uso de energia

--- sensor-temperatura.ino
+++ sensor-temperatura.ino
@@ -2,6 +2,7 @@
 #include <LCD.h>
 #include <LiquidCrystal_I2C.h>
 #include <DHT.h>
+#include <Narcoleptic.h>
 
 //Define a ligação ao pino de dados do sensor
 #define DHTPIN A3
@@ -9,9 +10,8 @@
 //Define o tipo de sensor DHT utilizado
 #define DHTTYPE DHT11
 
-DHT dht(DHTPIN, DHTTYPE);
-
-#define I2C_ADDR    0x27 // <<----- Add your address here.  Find it from I2C Scanner
+// I2C address - Find it from I2C Scanner.
+#define I2C_ADDR    0x27
 #define BACKLIGHT_PIN     3
 #define En_pin  2
 #define Rw_pin  1
@@ -21,13 +21,19 @@ DHT dht(DHTPIN, DHTTYPE);
 #define D6_pin  6
 #define D7_pin  7
 
+// Speaker pin
+#define SPK_PIN 12
+#define SPK_TONE 294
+#define SPK_VOL 30
+
+// Humidity sensor pin
 #define HUMIDITY_SENSOR_1 A0
 #define HUMIDITY_SENSOR_2 A1
 
+// Minimum humidity acceptable
 #define MIN_HUMIDITY 70
 
-int n = 1;
-
+DHT dht(DHTPIN, DHTTYPE);
 LiquidCrystal_I2C  lcd(I2C_ADDR, En_pin, Rw_pin, Rs_pin, D4_pin, D5_pin, D6_pin, D7_pin);
 
 //Array simbolo grau
@@ -43,11 +49,12 @@ byte grau[8] = { B00001100,
 
 void setup()
 {
-  Serial.begin(9600); //Inicializa a serial
+  Narcoleptic.disableSerial();
+  Narcoleptic.disableADC();
+  Narcoleptic.disableSPI();
 
-  lcd.begin (16, 2); //  <<----- My LCD was 16x2
-  lcd.clear(); //Limpa o LCD
-
+  lcd.begin (16, 2);
+  lcd.clear();
 
   // Switch on the backlight
   lcd.setBacklightPin(BACKLIGHT_PIN, POSITIVE);
@@ -56,49 +63,82 @@ void setup()
 
   //Cria o caractere customizado com o simbolo do grau
   lcd.createChar(0, grau);
+
+  // iniciando buzzer
+  pinMode(SPK_PIN, OUTPUT);
 }
 
 void loop()
 {
-  float h = dht.readHumidity(); //Le o valor da umidade
-  float t = dht.readTemperature(); //Le o valor da temperatura
-  
+  lcd.setBacklight(HIGH);
+
+  temp();
+
+  Narcoleptic.delay(8000);
+
+  lcd.clear();
+
+  humidity();
+
+  Narcoleptic.delay(5000);
+
+  sleep();
+}
+
+void beep() {
+  for (int x = 0; x < 5; x++) {
+    tone(SPK_PIN, SPK_TONE, SPK_VOL);
+    delay(500);
+  }
+}
+
+void sleep() {
+  lcd.clear();
+  lcd.setBacklight(LOW);
+
+  for (int pausas = 0; pausas <= 5; pausas++) {
+    Narcoleptic.delay(8000);
+  }
+}
+
+void temp() {
+  //Le o valor da umidade
+  float h = dht.readHumidity();
+
+  //Le o valor da temperatura
+  float t = dht.readTemperature();
+
   lcd.setCursor(0, 0);
   lcd.print("Temp : ");
   lcd.print(" ");
   lcd.setCursor(7, 0);
   lcd.print(t, 1);
-  lcd.setCursor(12, 0);
+  lcd.setCursor(SPK_PIN, 0);
 
   //Mostra o simbolo do grau formado pelo array
   lcd.write((byte)0);
-
-  //Mostra o simbolo do grau quadrado
-  //lcd.print((char)223);
 
   lcd.setCursor(0, 1);
   lcd.print("Umid : ");
   lcd.print(" ");
   lcd.setCursor(7, 1);
   lcd.print(h, 1);
-  lcd.setCursor(12, 1);
+  lcd.setCursor(SPK_PIN, 1);
   lcd.print("%");
+}
 
-  //Intervalo recomendado para leitura do sensor
-  delay(2000);
-
-  lcd.clear();
-
+void humidity() {
+  //Le os sensores de umidade
   int sensor1 = map(analogRead(HUMIDITY_SENSOR_1), 1023, 0, 0, 100);
   int sensor2 = map(analogRead(HUMIDITY_SENSOR_2), 1023, 0, 0, 100);
-  
+
   lcd.setCursor(0, 0);
   lcd.print("Sensor 1 : ");
   lcd.print(" ");
   lcd.setCursor(11, 0);
   lcd.print(sensor1);
   lcd.print("%");
-  
+
   lcd.setCursor(0, 1);
   lcd.print("Sensor 2 : ");
   lcd.print(" ");
@@ -106,7 +146,8 @@ void loop()
   lcd.print(sensor2);
   lcd.print("%");
 
-  delay(2000);
-
-  lcd.clear();
+  if (sensor1 >= MIN_HUMIDITY || sensor2 >= MIN_HUMIDITY) {
+    beep();
+  }
 }
+
